229cfa92021167fb2ec696d9a063b671a64a029d
Yang Zhang <zhangyang@yunba.io>
2016-03-10 16:19:06 +0800

    remove mnesia and resource_discovery

--- README.md
+++ README.md
@@ -17,7 +17,7 @@ rebar get-deps compile
 rebar eunit skip_deps=true
 
 # Run
-erl -pa ebin -pa deps/*/ebin -sname ha -setcookie abc -mnesia dir '"/tmp/mdb"' -s eredis_pool -s resource_discovery -s mnesia -s redis_hapool
+erl -pa ebin -pa deps/*/ebin -sname ha -setcookie abc -mnesia dir '"/tmp/mdb"' -s eredis_pool -s redis_hapool
 
 # APIs
 
--- src/redis_hapool_server.erl
+++ src/redis_hapool_server.erl
@@ -30,9 +30,6 @@
 
 -define(SERVER, ?MODULE).
 
--define(CHECK_MNESIA_TIME_INTERVAL, 2000).
--define(CHECK_MNESIA_TIMER, check_mnesia_timer).
-
 -define(REDIS_RECOVERY_TIME_INTERVAL, 2000).
 -define(REDIS_RECOVERY_TIMER, redis_recovery_timer).
 
@@ -79,19 +76,10 @@ init([RedisInfos]) ->
 
     {RedisConnections, InvalidConnections} = update_redis_connection_by_info(RedisInfos, []),
 
-    resource_discovery:add_local_resource_tuple({?REDISES_INFO_TABLE, node()}),
-    resource_discovery:add_target_resource_type(?REDISES_INFO_TABLE),
-    ok = resource_discovery:sync_resources(2500),
-
-    ClientNodes = resource_discovery:get_resources(?REDISES_INFO_TABLE),
-
-    create_redis_infos_table(lists:delete(node(), ClientNodes)),
     create_ets_redis_connections(),
 
-    update_mnesia_redis_info(RedisInfos),
     update_ets_redis_connections(RedisConnections),
 
-    %schedule_check_mnesia_redis_info(),
     schedule_redis_recovery(),
 
     lager:log(info, self(), "redis_pool started connection pools ~p", [RedisConnections]),
@@ -173,10 +161,9 @@ handle_cast(
                                        {add, N} -> OldInvalidConnections ++ N;
                                        {replace, N} -> N
                                    end,
-            %% changed really, do updte mnesia redis infos
+
             ToRedisInfos = [C#redis_connection.info || C <- ToRedisConnections],
             ?ERROR("changed connection from connections [~p], to connections [~p], infos [~p]", [FromRedisConnections, ToRedisConnections, ToRedisInfos]),
-            update_mnesia_redis_info(ToRedisInfos),
             update_ets_redis_connections(ToRedisConnections),
             {noreply, State#state{redis_infos = ToRedisInfos, redis_connections = ToRedisConnections, invalid_connections = ToInvalidConnections}};
         _ ->
@@ -202,28 +189,6 @@ handle_cast(_Request, State) ->
     {noreply, NewState :: #state{}} |
     {noreply, NewState :: #state{}, timeout() | hibernate} |
     {stop, Reason :: term(), NewState :: #state{}}).
-handle_info(?CHECK_MNESIA_TIMER, State = #state{redis_infos = OldRedisInfos, redis_connections = OldRedisConnections, invalid_connections = OldInvalidConnections}) ->
-    NewRedisInfos = case mnesia:dirty_read(?REDISES_INFO_TABLE, ?REDISES_INFO_TABLE_ID) of
-                        [{?REDISES_INFO_TABLE, ?REDISES_INFO_TABLE_ID, Infos}] -> Infos;
-    %% use old redis infos when mnesia failed
-                        MnesiaResult ->
-                            lager:log(error, self(), "mnesia: dirty_read failed [~p]", [MnesiaResult]),
-                            OldRedisInfos
-                    end,
-    ?DEBUG("checking mnesia redis infos", []),
-    case NewRedisInfos == OldRedisInfos of
-        true ->
-            %% redis info not changed
-            {noreply, State};
-        false ->
-            OldAllRedisConnection = OldRedisConnections ++ OldInvalidConnections,
-            %% redis info changed
-            {NewRedisConnections, NewInvalidConnections} = update_redis_connection_by_info(NewRedisInfos, OldAllRedisConnection),
-            ?ERROR("infos [~p] -> [~p], connections [~p] -> [~p]", [OldRedisInfos, NewRedisInfos, OldRedisConnections, NewRedisConnections]),
-            update_ets_redis_connections(NewRedisConnections),
-            {noreply, State#state{redis_infos = NewRedisInfos, redis_connections = NewRedisConnections, invalid_connections = NewInvalidConnections}}
-    end;
-
 handle_info(?REDIS_RECOVERY_TIMER, State = #state{redis_connections = OldRedisConnections, invalid_connections = OldInvalidConnections}) ->
     CheckInvalidConnectionResults = lists:map(
         fun(C) ->
@@ -375,65 +340,6 @@ new_connection_by_info(RedisInfo) ->
     lager:log(info, self(), "create pool result [~p]", [Result]),
     #redis_connection{info = RedisInfo, status = read_write}.
 
-%% mnesia operations
--spec (create_redis_infos_table(list()) -> ok).
-create_redis_infos_table([]) ->
-    case mnesia_table_exists(?REDISES_INFO_TABLE) of
-        false ->
-            mnesia:stop(),
-            mnesia:delete_schema([node()]),
-            mnesia:create_schema([node()]),
-            mnesia:start(),
-
-            case mnesia:create_table(?REDISES_INFO_TABLE,
-                %[{index, [#?REDISES_INFO_TABLE.info_id]}, {attributes, record_info(fields, ?REDISES_INFO_TABLE)}]) of
-                [{attributes, record_info(fields, ?REDISES_INFO_TABLE)}]) of
-                {atomic, ok} ->
-                    lager:log(info, self(), "mnesia: create table ~p", [?REDISES_INFO_TABLE]),
-                    ok;
-                {aborted, Reason} ->
-                    lager:log(error, self(), "mnesia: create table ~p fail: ~p", [?REDISES_INFO_TABLE, Reason]),
-                    error
-            end;
-        _ ->
-            ok
-    end;
-create_redis_infos_table(ClientNodes) ->
-    get_redis_pool_table_nodes(ClientNodes).
-
-mnesia_table_exists(TableName) ->
-    try
-        Tables = mnesia:system_info(tables),
-        lists:member(TableName,Tables)
-    catch
-        _->
-            false
-    end.
-
-get_redis_pool_table_nodes(Node) ->
-    case mnesia:change_config(extra_db_nodes, [Node]) of
-        {ok, [Node]} ->
-            mnesia:add_table_copy(?REDISES_INFO_TABLE, node(), ram_copies),
-            mnesia:wait_for_tables(mnesia:system_info(tables), infinity),
-            ok;
-        Error -> Error
-    end.
-
--spec (update_mnesia_redis_info(redis_info_list()) -> ok | {error, term()}).
-update_mnesia_redis_info(OkInfos) ->
-    F = fun() ->
-        mnesia:dirty_delete({?REDISES_INFO_TABLE, ?REDISES_INFO_TABLE_ID}),
-        mnesia:dirty_write(#?REDISES_INFO_TABLE{info_id = ?REDISES_INFO_TABLE_ID, infos = OkInfos}),
-        {ok}
-    end,
-    case mnesia:transaction(F) of
-        {atomic, ResultOfFun} ->
-            ResultOfFun;
-        {aborted, Reason} ->
-            lager:log(error, self(), "update mnesia redis info failed [~p]", [Reason]),
-            {error, Reason}
-    end.
-
 
 -spec (create_ets_redis_connections() -> ok | {error, term()}).
 create_ets_redis_connections() ->
@@ -449,10 +355,6 @@ update_ets_redis_connections(OkConnections) ->
         lager:log(error, self(), "update ets redis connection failed[~p:~p]", [E, T])
     end.
 
--spec (schedule_check_mnesia_redis_info() -> ok).
-schedule_check_mnesia_redis_info() ->
-    timer:send_interval(?CHECK_MNESIA_TIME_INTERVAL, ?SERVER, ?CHECK_MNESIA_TIMER).
-
 -spec (schedule_redis_recovery() -> ok).
 schedule_redis_recovery() ->
     timer:send_interval(?REDIS_RECOVERY_TIME_INTERVAL, ?SERVER, ?REDIS_RECOVERY_TIMER).
@@ -461,8 +363,7 @@ schedule_redis_recovery() ->
 -spec (is_redis_valid(#redis_connection{}) -> ok).
 is_redis_valid(Connection) ->
     PoolName = redis_hapool_server:poolname(Connection),
-    TestKeys = lists:seq(1, ?CHECK_REDIS_KEY_COUNT),
-    Result = eredis_pool:q(PoolName, ["MGET"] ++ TestKeys),
+    Result = eredis_pool:q(PoolName, ["GET", "1"]),
     case Result of
         {ok, _ListResult} ->
             true;
